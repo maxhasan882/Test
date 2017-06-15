42d064ef060296cc68a48f16552463aaa1c7554f
Rijoanul Hasan Shanto <rijoanul.shanto@gmail.com>
2017-06-15 21:45:21 +0600

    Add files via upload

---
+++ default.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <vector>
+#define IN freopen("f:\\in.txt", "rt", stdin);
+#define OUT freopen("f:\\out.txt", "wt", stdout);
+#define FOR(n) for(int i = 0; i < n; i++)
+#define LL long long int
+#define STESTCASE int t;scanf("%d", &t);getchar();for(int tc = 1; tc <= t; tc++)
+#define TESTCASE int t;scanf("%d", &t);for(int tc = 1; tc <= t; tc++)
+#define PF printf
+#define SF scanf
+#define EP 0.000000001
+#define MAX 1000
+using namespace std;
+
+int main()
+{
+    cout << "hello robert" << endl;
+
+    return 0;
+}
